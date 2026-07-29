e23c1cd552d1eea2d1dfa3161b3c3420fa6af979
rnparave <rnparave7@gmail.com>
2026-07-29 23:53:02 +0530

    Add system info Script

---
+++ system_info.sh
@@ -0,0 +1,23 @@
+echo "====================================================================================="
+echo "                               SYSTEM INFORMATION REPORT                             "
+echo "====================================================================================="
+echo ""
+echo "Date and Time: $(date)"
+echo "Logged in as: $(whoami)"
+echo "The system is been running for $(uptime -p)"
+echo "-----General Computer system information-----"
+hostnamectl
+echo ""
+echo "----------DISK USAGE----------"
+df -h
+echo ""
+echo "---------MEMORY USAGE---------"
+free -h
+echo ""
+echo "---RUNNING PROCESSES (top 5)---"
+ps aux --sort=-%cpu | head -6
+echo ""
+echo "-----Overall CPU Percentage-----"
+top -bn1 | grep "Cpu(s)"
+echo "====================================================================================="
+
